1868b18f668df6b54f49eebaaea2e42b0db109a7
Josip Lovrić <70754640+Lovric348@users.noreply.github.com>
2023-07-02 14:10:46 +0200

    8th commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
 import 'package:bloc_learning/presentation/screens/fun_page.dart';
+import 'package:bloc_learning/service/bored_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'bloc/colors/bloc/colors_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+ 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FunPage();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ColorsBloc(),
+        ),
+        
+      ],
+      child: FunPage()
+      
+    );
   }
+
+  
 }
--- lib/presentation/screens/fun_page.dart
+++ lib/presentation/screens/fun_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../bloc/colors/bloc/colors_bloc.dart';
 
-
-
 class FunPage extends StatelessWidget {
   const FunPage({Key? key}) : super(key: key);
 
@@ -16,8 +14,13 @@ class FunPage extends StatelessWidget {
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: BlocBuilder<ColorsBloc, ColorsState>(
-         builder: (BuildContext context, state) {
+            builder: (BuildContext context, state) {
           return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              onPressed: (){
+                Navigator.pushNamed(context, '/seriouspage');
+                
+              }),
             backgroundColor: state.color,
             appBar: AppBar(
               backgroundColor: state.color,
@@ -27,102 +30,110 @@ class FunPage extends StatelessWidget {
                 style: TextStyle(
                   color: state.colorTitle,
                   fontSize: 29,
-                ),),
-            ),
-            body: Container(
-
-              height: screenHeight*1,
-              width: screenWidth*1,
-              child: Stack(
-                children: [
-                  
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Text(
-                      'How many news would \nyou like to read?',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 27,
-                        fontWeight: FontWeight.bold,
-                        color: state.colorTitle,
-                      ),)),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      width: screenWidth*0.8,
-                      height: screenHeight*0.6,
-                      child: Stack(
-                        children: [
-                        SizedBox(
-                          width: screenWidth*1,
-                          height: screenHeight*1,
-                          child: FloatingActionButton(
-                            child: Text(
-                              "${state.number}",
-                              style: TextStyle(
-                                fontSize: 29,
-                                fontWeight: FontWeight.bold,
-                              ) ,),
-                            onPressed: (){
-                              context.read<ColorsBloc>().add(ColorChangeEvent());
-                            },
-                            splashColor: Color.fromARGB(255, 6, 84, 147),
-                            ),
-                        ),
-                        SizedBox(
-                          
-                          width: screenWidth*0.3,
-                          height: screenHeight*0.3,
-                          child: FloatingActionButton(
-                            child: Text(
-                              '0',
-                              style: TextStyle(
-                                fontSize: 29,
-                                fontWeight: FontWeight.bold,
-                              ),),
-                            splashColor: Colors.black87,
-                            elevation: 5,
-                            backgroundColor: Colors.grey,
-                            onPressed: (){
-                              context.read<ColorsBloc>().add(ResetEvent());
-                              }
-                            ),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: SizedBox(
-                            
-                            width: screenWidth*0.4,
-                            height: screenHeight*0.4,
-                            child: FloatingActionButton(
-                              child: Text(
-                                '-1',
-                                style: TextStyle(
-                                  fontSize: 29,
-                                  fontWeight: FontWeight.bold,
-                                ),),
-                              splashColor: Color.fromARGB(221, 9, 101, 29),
-                              elevation: 5,
-                              backgroundColor: Color.fromARGB(255, 140, 239, 104),
-                              onPressed: (){
-                                context.read<ColorsBloc>().add(DecreaseEvent());
-                                }
-                              ),
-                          ),
-                        ),
-                          ],
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
+            body: Container(
+              height: screenHeight * 1,
+              width: screenWidth * 1,
+              child: 
+                  Stack(
+                    children: [
+                      Align(
+                          alignment: Alignment.topCenter,
+                          child: Text(
+                            'How many news would \nyou like to read?',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 27,
+                              fontWeight: FontWeight.bold,
+                              color: state.colorTitle,
+                            ),
+                          )),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          width: screenWidth * 0.8,
+                          height: screenHeight * 0.6,
+                          child: Stack(
+                            children: [
+                              SizedBox(
+                                width: screenWidth * 1,
+                                height: screenHeight * 1,
+                                child: FloatingActionButton(
+                                  child: Text(
+                                    "${state.number}",
+                                    style: TextStyle(
+                                      fontSize: 29,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    context
+                                        .read<ColorsBloc>()
+                                        .add(ColorChangeEvent());
+                                  },
+                                  splashColor: Color.fromARGB(255, 6, 84, 147),
+                                ),
+                              ),
+                              Container(
+                                width: screenWidth * 0.3,
+                                height: screenHeight * 0.2,
+                                child: FloatingActionButton(
+                                    child: Text(
+                                      '0',
+                                      style: TextStyle(
+                                        fontSize: 29,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    splashColor: Colors.black87,
+                                    elevation: 5,
+                                    backgroundColor: Colors.grey,
+                                    onPressed: () {
+                                      context
+                                          .read<ColorsBloc>()
+                                          .add(ResetEvent());
+                                    }),
+                              ),
+                              Align(
+                                alignment: Alignment.bottomRight,
+                                child: Container(
+                                  width: screenWidth * 0.4,
+                                  height: screenHeight * 0.4,
+                                  child: FloatingActionButton(
+                                      child: Text(
+                                        '-1',
+                                        style: TextStyle(
+                                          fontSize: 29,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      splashColor:
+                                          Color.fromARGB(221, 9, 101, 29),
+                                      elevation: 5,
+                                      backgroundColor:
+                                          Color.fromARGB(255, 140, 239, 104),
+                                      onPressed: () {
+                                        context
+                                            .read<ColorsBloc>()
+                                            .add(DecreaseEvent());
+                                            
+                                      }),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  
+                
+            ),
           );
-
-         }),
-      
+        }),
       ),
     );
-    
   }
 }
