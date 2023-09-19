d2fb47fa8d5bcba3edda686b3a1a01bf0a1fa1d1
Josip Lovrić <70754640+Lovric348@users.noreply.github.com>
2023-09-19 14:46:20 +0200

    stuff deleted

--- lib/models/hastag_model.dart
+++
@@ -1,6 +0,0 @@
-class Shop {
-  final List<String> hastags = [];
-  final String name = '';
-  
-  Shop(this.name, this.hastags);
-}
--- lib/presentation/screens/practice_screen.dart
+++ lib/presentation/screens/practice_screen.dart
@@ -1,8 +1,5 @@
-import 'package:bloc_learning/bloc/bloc/smjesa_bloc.dart';
-import 'package:bloc_learning/bloc/povrda/potvrda_bloc.dart';
-import 'package:bloc_learning/models/custom_text_field.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PracticeHomeScreen extends StatelessWidget {
  
@@ -10,50 +7,8 @@ class PracticeHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: BlocBuilder<PotvrdaBloc, PotvrdaState>(
-        builder: (context, state) {
-          return SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                customTextField('email',
-                    (value){
-                      context.read<PotvrdaBloc>().add(EmailEvent(value));
-                    } 
-                     
-                ),
-                SizedBox(height: 70,),
-                customTextField('Password',
-                (value){
-                      context.read<PotvrdaBloc>().add(PasswordEvent(value));
-                    } 
-                ),
-                Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Container(
-                    height: 20,
-                    width: 120,
-                    child: TextField(
-                      onChanged: (value) {
-                        context.read<SmjesaBloc>().add(HastagSearch(searchTerm: value));
-                      },
-                    ),
-                  ),
-                  )
-                /* customTextField('Search',
-                (value){
-                      context.read<SmjesaBloc>().add(HastagSearch(searchTerm: value));
-                      
-                    } 
-                
-                ), */
-                
-                
-              ],
-            ),
-          );
-        },
-      )),
+      child: Scaffold(body: Container(),
+      ),
     );
   }
 }
