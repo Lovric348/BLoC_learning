43294e5c101bbcc7a8efcd8cb5ae22c8a98637bd
Josip Lovrić <70754640+Lovric348@users.noreply.github.com>
2023-10-23 09:02:49 +0200

    zadnje promjene

--- lib/presentation/screens/korisnik.dart
+++ lib/presentation/screens/korisnik.dart
@@ -1,15 +1,15 @@
 import 'package:bloc_learning/models/korisnik.dart';
+import 'package:bloc_learning/widgets/korisnik_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class KorisnikHomeScreen extends StatefulWidget {
-  List<Korisnik> korisnici =
   
-  [Korisnik(ime: 'Mato', prezime: 'Basic', godine: 28),
-  Korisnik(ime: 'Pejo', prezime: 'Crnoja', godine: 38),
-  Korisnik(ime: 'Ivo', prezime: 'Jaje', godine: 23)
-  ]; 
+  /* [
+    Korisnik(ime: 'Mato', prezime: 'Basic', godine: 28),
+    Korisnik(ime: 'Pejo', prezime: 'Crnoja', godine: 38),
+    Korisnik(ime: 'Ivo', prezime: 'Jaje', godine: 23)
+  ]; */
   KorisnikHomeScreen({super.key});
 
   @override
@@ -17,19 +17,31 @@ class KorisnikHomeScreen extends StatefulWidget {
 }
 
 class _KorisnikHomeScreenState extends State<KorisnikHomeScreen> {
-  final korisnik = FirebaseAuth.instance.currentUser!;
-
-  List<String> docIds= [];
-  Future getDocIds() async{
-    QuerySnapshot querySnapshot= await FirebaseFirestore.instance.collection('korisnici').get();
-    print(querySnapshot);
+  List<Korisnik> korisnici = [];
+  Future<Korisnik> getDocIds() async {
+    String ime = '';
+    String prezime = '';
+    int godine = 0;
+    try{
+    DocumentSnapshot docSnapshot = await FirebaseFirestore.instance.collection('korisnici').doc().get();
+    ime = docSnapshot['ime'];
+    prezime = docSnapshot['prezime'];
+    godine = docSnapshot['godine'];
+    }
+        
+    on FirebaseException {
+    // sto ako bude error
   }
+  return Korisnik(ime: ime, prezime: prezime, godine: godine);
+  }
+  
 
   @override
   void initState() {
     getDocIds();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -37,15 +49,15 @@ class _KorisnikHomeScreenState extends State<KorisnikHomeScreen> {
         body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Container(
-             child: ListView.builder(
-              itemCount: 40,
-              itemBuilder: (BuildContext context, int index){
-                return Text('kads');
-              }
-               
-               ),
+            child: ListView.builder(
+              itemCount: korisnici.length,
+              itemBuilder: (BuildContext context, index) {
+                return KorisnikWidget(korisnik: korisnici[index]);
+              },
+            ),
           ),
         ),
-      ),);
+      ),
+    );
   }
-}
+} 
